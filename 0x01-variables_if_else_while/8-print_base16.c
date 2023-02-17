cafa2629f26d1e4d17c7cac09135647ff91b3385
Shaimaaabdelmoaen <sa3073@fayoum.edu.eg>
2023-02-17 14:19:32 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - Entry - point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+	char d;
+
+	c = 0;
+	d = 'a';
+	while
+		(c < 10) {
+			putchar(c);
+			c++;
+		}
+	while
+		(d < 'g') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
