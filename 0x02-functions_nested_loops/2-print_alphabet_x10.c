6c7da88b1b023afbf16b0213a42cc93ef9bf4bc2
Shaimaaabdelmoaen <sa3073@fayoum.edu.eg>
2023-02-21 08:21:21 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry point
- * Description: program that prints alphabet in lowercase
+ * print_alphabet_x10 - entry point
+ * Description: program that prints alphabet in lowercase 10 times
+ *
  * Return: 0
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	int i;
-	int j;
+	int alpha;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i <= 9)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
-			_putchar(j);
+			_putchar(alpha);
 		}
 		_putchar('\n');
-	}
+
+		i++;
+			}
 }
